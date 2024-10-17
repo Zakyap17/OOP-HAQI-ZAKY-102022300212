19960b46d5e69260e2e05313631bf78fa2cd1b3c
Zakyap17 <zakyaprilian17@gmail.com>
2024-10-17 08:59:30 +0700

    Add files via upload

---
+++ MODUL1_JURNAL_ZAKY/src/Makanan.java
@@ -0,0 +1,19 @@
+// Don't delete any comments below!!!
+
+public class Makanan {
+    // Todo : Create private attribute of Makanan (nama, jumlah, and harga)
+        String name;
+        int jumlah;
+        double harga;
+    // Todo : Create Constructor of Makanan
+public class makanan {String name, int jumlah, double harga {
+    this.name = name;
+    this.jumlah;
+    this.harga;
+}   
+
+}
+
+    // Todo : Create Getter and Setter
+
+}
